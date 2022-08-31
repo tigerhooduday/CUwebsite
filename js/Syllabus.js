46d7f11db660cc4e22910317982777f6705abc7e
zirck Trons <zirck@localhost.localdomain>
2022-08-31 15:05:57 +0530

    UPDATE1.2

--- js/Syllabus.js
+++ js/Syllabus.js
@@ -1,3 +1,117 @@
 function Home() {
     window.location.href="../";
 }
+
+//----------------------------------------------
+    //               daa --- unit 1
+//--------------------------------------------
+
+
+/*
+
+//------------------------
+    //ppt 1
+function ppt1_1(){
+    
+    document.getElementById("ppt1_1").style.visibility = "visible";
+    document.getElementById("ppt1_1").style.height = "542px";
+    
+
+
+
+}
+    // stop
+function pptx(){
+    document.getElementById("ppt1_1").style.visibility = "hidden";
+
+}
+
+//------------------------
+
+  //ppt 1
+function ppt1_2(){
+
+    document.getElementById("ppt1_2").style.visibility = "visible";
+    document.getElementById("ppt1_2").style.height = "542px";
+    
+
+}
+    //stop
+function pptx1_2(){
+    document.getElementById("ppt1_2").style.visibility = "hidden";
+}
+*/
+function pptx()
+{
+    document.getElementById("frame1").style.visibility = "hidden";
+    document.getElementById("ppthead").style.visibility = "hidden";
+
+
+}
+
+function ppt1_1(){
+    document.getElementById("frame1").style.visibility = "visible";
+    document.getElementById("ppthead").style.visibility = "visible";
+    document.getElementById("ppthead").innerHTML = "PPT 1.1";
+    document.getElementById("frame1").src="https://drive.google.com/file/d/1cMzCXoyFtRw9jFmXqg9kzoX2Sgy6jhQw/preview";
+}
+
+//-----------  PPT 2
+function ppt1_2(){
+    document.getElementById("frame1").style.visibility = "visible";
+    document.getElementById("ppthead").style.visibility = "visible";
+    document.getElementById("ppthead").innerHTML = "PPT 1.2";
+    document.getElementById("frame1").src="https://drive.google.com/file/d/1hipIBygSERRy-JxsiKoPvXOxfF9V29Q8/preview";
+}
+
+//-----------  PPT 3 
+function ppt1_3(){
+    document.getElementById("frame1").style.visibility = "visible";
+    document.getElementById("ppthead").style.visibility = "visible";
+    document.getElementById("ppthead").innerHTML = "PPT 1.3 - PPT 1.5";
+    document.getElementById("frame1").src="https://drive.google.com/file/d/1YoYNo1gwxmIYRxjw3oA8e2BK04Bb8Stz/preview";
+}
+
+//-----------  PPT 1.5 
+function ppt1_5(){
+    document.getElementById("frame1").style.visibility = "visible";
+    document.getElementById("ppthead").style.visibility = "visible";
+    document.getElementById("ppthead").innerHTML = "Lecture topic 1.5";
+    document.getElementById("frame1").src="https://drive.google.com/file/d/1R10-HsoUg3q5K2lL0hmEJOz7dT_3s8zD/preview";
+}
+
+
+
+//-----------  PPT 2.1
+function ppt2_1(){
+    document.getElementById("frame1").style.visibility = "visible";
+    document.getElementById("ppthead").style.visibility = "visible";
+    document.getElementById("ppthead").innerHTML = "Lecture topic 2.1";
+    document.getElementById("frame1").src="https://drive.google.com/file/d/1CLrLpeVykWzHXq8QN9ndEjD-l94C5Rpt/preview";
+}
+
+
+
+//-----------  PPT 2.2
+function ppt2_2(){
+    document.getElementById("frame1").style.visibility = "visible";
+    document.getElementById("ppthead").style.visibility = "visible";
+    document.getElementById("ppthead").innerHTML = "Lecture topic 2.2";
+    document.getElementById("frame1").src="https://drive.google.com/file/d/1HwJsJiFNo4Zx665oleP8hY3BXYXyK_MU/preview";
+}
+
+//-----------  PPT 2.3
+function ppt2_3(){
+    document.getElementById("frame1").style.visibility = "visible";
+    document.getElementById("ppthead").style.visibility = "visible";
+    document.getElementById("ppthead").innerHTML = "Lecture topic 2.3";
+    document.getElementById("frame1").src="https://drive.google.com/file/d/1HwJsJiFNo4Zx665oleP8hY3BXYXyK_MU/preview";
+}
+
+//-----------  PPT 2.4
+function ppt2_3(){
+    document.getElementById("frame1").style.visibility = "visible";
+    document.getElementById("ppthead").style.visibility = "visible";
+    document.getElementById("ppthead").innerHTML = "Lecture topic 2.4";
+    document.getElementById("frame1").src="https://drive.google.com/file/d/1QghNNDBLGix-jj7iBJu_9KSpM6yvVi3v/preview";
+}
